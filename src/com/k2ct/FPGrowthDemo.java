16b7f5ec9c27abae012986bf81751d79aca0aa5b
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2022-05-10 17:06:07 +0700

    New params config for cluster 1, 2, 3

--- src/com/k2ct/FPGrowthDemo.java
+++ src/com/k2ct/FPGrowthDemo.java
@@ -20,21 +20,21 @@ public class FPGrowthDemo {
 
 
         FpGrowth model_cluster1 = new FpGrowth("data/cluster_1_trans_data.arff",
-                "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.03 -S", "-R 1");
+                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.02 -S", "-R 1");
         model_cluster1.mineAssociationRules();
         model_cluster1.saveFrequentItemSet("data/frequent_itemsets_cluster1.csv");
         System.out.println("Association Rules for cluster 1 data" + model_cluster1);
 
 
         FpGrowth model_cluster2 = new FpGrowth("data/cluster_2_trans_data.arff",
-                "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.03 -S", "-R 1");
+                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.012 -S", "-R 1");
         model_cluster2.mineAssociationRules();
         model_cluster2.saveFrequentItemSet("data/frequent_itemsets_cluster2.csv");
         System.out.println("Association Rules for cluster 2 data" + model_cluster2);
 
 
         FpGrowth model_cluster3 = new FpGrowth("data/cluster_3_trans_data.arff",
-                "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.03 -S", "-R 1");
+                "-P 2 -I -1 -N 10 -T 0 -C 0.7 -D 0.05 -U 1.0 -M 0.012 -S", "-R 1");
         model_cluster3.mineAssociationRules();
         model_cluster3.saveFrequentItemSet("data/frequent_itemsets_cluster3.csv");
         System.out.println("Association Rules for cluster 3 data" + model_cluster3);
